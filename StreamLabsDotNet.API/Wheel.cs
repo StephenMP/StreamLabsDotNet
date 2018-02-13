6f2e2fe4aeb7dfdac019d2814e8be0f218e863b4
Andy Morrell <andy@andymorrell.net>
2018-02-13 15:37:15 +0000

    Credits, Jar and Wheel API added

---
+++ StreamLabsDotNet.API/Wheel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StreamLabsDotNet.API
+{
+    public class Wheel : ApiBase
+    {
+        public async Task<SuccessResponse> SpinWheelAsync(string accessToken)
+        {
+            if (string.IsNullOrWhiteSpace(accessToken)) throw new BadParameterException("Access token is not valid. It is not allowed to be null, empty or filled with whitespaces.");
+
+            var url = "wheel/spin";
+            var payload = new Dictionary<string, object> {
+                { "access_token",accessToken}
+            };
+
+            return JsonConvert.DeserializeObject<SuccessResponse>((await GeneralRequestAsync(url, "POST", payload, RequestType.Query).ConfigureAwait(false)).Value);
+        }
+
+    }
+}
