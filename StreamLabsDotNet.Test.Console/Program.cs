f836e34d918f11811febe45fe18dab54c23b23a7
StephenMP <1.stephen.porter@gmail.com>
2018-09-08 08:29:22 -0600

    Add await operator

--- StreamLabsDotNet.Test.Console/Program.cs
+++ StreamLabsDotNet.Test.Console/Program.cs
@@ -1,22 +1,22 @@
-﻿using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-
-using System;
+﻿using System;
 using System.Threading;
 using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 namespace StreamLabsDotNet.Test.Console
 {
     class Program
     {
 
-        public static void Main(string[] args)
-     => new Program().MainAsync().GetAwaiter().GetResult();
+        public static void Main(string[] args) => new Program().MainAsync().GetAwaiter().GetResult();
 
         private static Mutex mutex = new Mutex(true, "{886D4E8D-0962-4174-85FC-FA36C0FFCB6D}");
         private Client.Client _client;
         public async Task MainAsync()
         {
+            await Task.CompletedTask;
+
             if (mutex.WaitOne(TimeSpan.Zero, true))
             {
                 var socketToken = "{{SocketToken}}";
@@ -24,7 +24,7 @@ namespace StreamLabsDotNet.Test.Console
                 var serviceCollection = new ServiceCollection()
                .AddLogging()
                .AddSingleton<Client.Client>();
-                
+
                 var serviceProvider = serviceCollection.BuildServiceProvider();
 
                 serviceProvider
